945278f7f27fdeb001f43fc1744c85b6b632c39c
Alexander Eberle <alexander171294@gmail.com>
2021-11-08 13:47:57 -0300

    getting connections from pool directly

--- src/main/java/net/hirana/services/Database.java
+++ src/main/java/net/hirana/services/Database.java
@@ -17,7 +17,6 @@ public enum Database {
     private Connection conn;
 
     public void init() throws SQLException {
-//        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
         config.setJdbcUrl( getUrl(System.getenv("DB_HOST"), Integer.parseInt(System.getenv("DB_PORT")), System.getenv("DB_NAME")) );
         config.setUsername( System.getenv("DB_USERNAME"));
         config.setPassword( System.getenv("DB_PASSWORD") );
@@ -33,15 +32,8 @@ public enum Database {
         return String.format("jdbc:mysql://%s:%d/%s", host, port, dbName);
     }
 
-    public Connection getConnection() throws SQLException {
-        if(this.conn == null) {
-            this.conn = ds.getConnection();
-        }
-        return this.conn;
-    }
-
     public String getUserPassword(String user) throws SQLException {
-        Connection con = this.getConnection();
+        Connection con = ds.getConnection();
         PreparedStatement pst = con.prepareStatement(String.format("SELECT pass FROM %s WHERE LOWER(display) = '%s' LIMIT 1", tableName, user.toLowerCase()));
         ResultSet rs = pst.executeQuery();
         if(rs.next()) {
